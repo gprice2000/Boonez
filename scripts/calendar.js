d735d46ccd627dc540788c6831552006a7f68367
Jacob Mazzarese <76662555+JacobMazzarese@users.noreply.github.com>
2022-03-31 20:12:03 -0400

    Added calendar and Profile pic functionality

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,6 +1,104 @@
+var cur_cal;
+
 document.addEventListener('DOMContentLoaded', function() {
 	var calendarEl = document.getElementById('calendar');
+	publicCal(calendarEl);
+	// When the user clicks on the button, open the modal
+	document.getElementById("CalBtn")
+			.addEventListener("click", function() {
+				var btn_val = document.getElementById("CalBtn").innerHTML; 
+				if (btn_val == "Public") {
+					document.querySelector("#CalBtn").innerHTML = "Private";
+					publicCal(calendarEl);
+				}
+				else {
+					document.querySelector("#CalBtn").innerHTML = "Public";
+					privateCal(calendarEl);
+				}
+			});
+	
+});
 
+function dateFormat(date) {
+	if (date == null) return null;
+	var month = date.getMonth() + 1; //months from 1-12
+	var day = date.getDate();
+	var year = date.getFullYear();
+	console.log("month length" + month.toString().length)
+	console.log("day length" + day.length)
+
+	if (month.toString().length != 2) {
+		month = '0'.concat(month);
+	}
+	if (day.toString().length != 2) {
+		day = '0'.concat(day);
+	}
+	return year + "-" + month + "-" + day;
+}
+function privateCal(calendarEl) {
+		var calendar = new FullCalendar.Calendar(calendarEl, {
+			headers: {
+				left: 'prev,next today',
+			},
+			selectable: true,
+			eventClick: function(calEvent) {
+				console.log(calEvent.start)
+				let event = { title: calEvent.event.title,
+					id: parseInt(calEvent.event.id),
+					start: dateFormat(calEvent.event.start),
+				    end: dateFormat(calEvent.event.end) }
+				editEvent('pri',event);
+			},
+			eventSources: [
+				{
+					url: '/Priv_calendar',
+					method: 'GET',
+					success: function(res) {
+						console.log("Calendar Events received:");
+					},
+					failure: function() {
+						return;
+					}
+				}
+			],
+
+			select: function(info) {
+				var title = prompt('Event Title:');
+				if (title) {
+					let new_event = 
+					{
+						id: genID(),
+						title: title,
+						start: info.startStr,
+						end: info.endStr,
+					};
+					this.addEvent(new_event);
+					fetch('http://localhost:3000/Priv_calendar', {
+						method: 'POST', 
+						credentials: 'same-origin',
+						mode: 'same-origin',
+						headers: {
+							'Content-Type' : 'application/json',
+						},
+						body: JSON.stringify(new_event),
+					})
+					.then(response => response.json())
+					.then(new_event => {
+						console.log('Success:');
+					})
+					.catch((error) => {
+						console.error("Error:");
+					});
+				}
+
+			}
+		});
+		cur_cal = calendar;
+		calendar.render();
+
+}
+
+function publicCal(calendarEl) {
 	var calendar = new FullCalendar.Calendar(calendarEl, {
 		headers: {
 			left: 'prev,next today',
@@ -8,37 +106,49 @@ document.addEventListener('DOMContentLoaded', function() {
 			right: 'month,agendaWeek,agendaDay'
 		},
 		selectable: true,
-		/*
-			Add an eventSources: so we can first check the data
-			base if the user has any saved events, if not we 
-			simply send an empty events object.
-		*/
+		timeFormat: 'YYYY, MM, DD',
+		eventClick: function(calEvent) {
+			// Get the modal
+			//if (calEvent.end == null)
+			console.log(calEvent)
+			let event = { title: calEvent.event.title,
+				id: parseInt(calEvent.event.id),
+				start: dateFormat(calEvent.event.start),
+				end: dateFormat(calEvent.event.end) }
+				console.log(event)
+
+			editEvent('pub',event);
+			console.log("PUBLIC EVENT HAS BEEN CLICKED");
+
+		},
 		eventSources: [
 			{
-				url: '/calendar',
+				url: '/Pub_calendar',
 				method: 'GET',
 				success: function(res) {
-					console.log("TEST");
+					pub_arr = res 
+					console.log("Calendar Events received:");
 				},
 				failure: function() {
 					alert("There was an error while fetching events");
 				}
 			}
 		],
+
 		select: function(info) {
 			var title = prompt('Event Title:');
-			console.log(info.startStr);
-			console.log(info.endStr);
+			
 			if (title) {
 				let new_event = 
 				{
+					id: genID(),
 					title: title,
 					start: info.startStr,
 					end: info.endStr,
 				};
 				this.addEvent(new_event);
 
-				fetch('http://localhost:3000/calendar', {
+				fetch('http://localhost:3000/Pub_calendar', {
 					method: 'POST', 
 					credentials: 'same-origin',
 					mode: 'same-origin',
@@ -55,9 +165,84 @@ document.addEventListener('DOMContentLoaded', function() {
 					console.error("Error:");
 				});
 			}
+		}
+	});
+	cur_cal = calendar;
+	calendar.render();
+}
+function genID() {
+	return (cur_cal.getEvents()).length;
+}
 
+function editEvent(cal_type, info) {
+
+	var modal = document.getElementById("myModal");
+	modal.style.display = "block";
+	let id = info.id;
+	var subBtn = document.getElementById("calSub");
+	var delBtn = document.getElementById("calDel");
+	let t_q = document.querySelector("#Title");
+	let s_q = document.querySelector("#EventStart");
+	let e_q = document.querySelector("#EventEnd");
+	t_q.value = info.title;
+	s_q.value = info.start;
+	e_q.value = info.end; 
+	subBtn.onclick = function() {
+
+		let title = t_q.value;
+		let start = s_q.value;
+		let end = e_q.value;
+		
+		console.log("")
+		if (title == '') {
+			title = info.title;
+		}
+		if (start == '' ) {
+			start = info.start;
+		}
+		if (end != '' && start > end) {
+			window.alert("Error: Start date has to come before end.");
 		}
 
+		console.log(title + "\n" + start + "\n" + end)
+		serverCon('POST',{id,cal_type,title,start,end},'/editEvent');
+
+	}
+	delBtn.onclick = function() {
+		var obj = {id,info, cal_type};
+		serverCon('DELETE',obj,'/deleteEvent');
+	}
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+	cur_cal.render();
+}
+
+function serverCon(method, data,url) {
+	fetch('http://localhost:3000'+url, {
+		method: method, 
+		credentials: 'same-origin',
+		mode: 'same-origin',
+		headers: {
+			'Content-Type' : 'application/json',
+		},
+		body: JSON.stringify({data}),
+	})
+	.then(response => response.json())
+	.then(new_event => {
+		console.log('Success:');
+	})
+	.catch((error) => {
+		console.error("Error:");
 	});
-	calendar.render();
-});
+}
--- server.js
+++ server.js
@@ -8,23 +8,19 @@ const mongodb = require("mongodb");
 const bcrypt = require("bcryptjs");
 const bodyParser = require("body-parser");
 const path = require("path");
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
 const sessions = require("express-session");
 const { getSystemErrorMap } = require("util");
+//const { join } = require("path/posix");
 const favicon = require("serve-favicon");
-
 let session = { userid: "" };
-//load favicon
 app.use(favicon(__dirname + "/favicon.ico"));
-//connect to database
+
 const db = mongodb.MongoClient.connect(
   "mongodb+srv://mazzaresejv:B00nze2022@cluster0.awpng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 );
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-//set one day time for cookies to expire
-//setting up express-sessions
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(
   sessions({
@@ -34,21 +30,16 @@ app.use(
     resave: false,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
 //set up cookie parser
 app.use(cookieParser());
-// let session = { userid: null };
+//used for keeping track of messages
+let date_ob = new Date();
 
-//serving favicon
-let date_ob = new Date(); //used for keeping track of messages
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(
-//   express.static(path.join(__dirname + "/New_capstone/Boonez-landing_pages"))
-// );
 let messageRecipient;
 function socketIOConnection() {
   let userId = session.userid;
@@ -79,49 +70,48 @@ function socketIOConnection() {
     });
   });
 }
+
 app.post("/signup", function (req, res) {
   db.then(function (dbc) {
     dbc
-      .db("Boonez")
-      .collection("profiles")
-      .findOne({ username: req.body.username }, function (err, result) {
-        if (result) {
-          res.send("username already in use");
-        } else {
-          dbc
-            .db("Boonez")
-            .collection("profiles")
-            .findOne({ email: req.body.email }, function (err, result) {
-              if (result) {
-                res.send("email already in use");
-              } else {
-                let pass = req.body.password;
-                bcrypt.genSalt(10, function (saltError, salt) {
-                  if (saltError) {
-                    throw saltError;
-                  } else {
-                    bcrypt.hash(pass, salt, function (hashError, hash) {
-                      if (hashError) {
-                        throw hashError;
-                      }
-                      req.body.password = hash;
-                      let input = req.body;
-                      input.friendsList = [];
-                      console.log(input);
-                      dbc
-                        .db("Boonez")
-                        .collection("profiles")
-                        .insertOne(req.body);
-                      res.redirect("/login");
-                    });
-                  }
-                });
-              }
-            });
-        }
-      });
+    .db("Boonez")
+    .collection("profiles")
+    .findOne({ username: req.body.username }, function (err, result) {
+      if (result) {
+        res.send("username already in use");
+      } else {
+        dbc
+          .db("Boonez")
+          .collection("profiles")
+          .findOne({ email: req.body.email }, function (err, result) {
+            if (result) {
+              res.send("email already in use");
+            } else {
+              let pass = req.body.password;
+              bcrypt.genSalt(10, function (saltError, salt) {
+                if (saltError) {
+                  throw saltError;
+                } else {
+                  bcrypt.hash(pass, salt, function (hashError, hash) {
+                    if (hashError) {
+                      throw hashError;
+                    }
+                    req.body.password = hash;
+                    dbc
+                      .db("Boonez")
+                      .collection("profiles")
+                      .insertOne(req.body);
+                    res.redirect("/login");
+                  });
+                }
+              });
+            }
+          });
+      }
+    });
   });
 });
+
 app.post("/login", function (req, res) {
   db.then(function (dbc) {
     dbc
@@ -132,27 +122,25 @@ app.post("/login", function (req, res) {
           console.log("Unable to locate account");
           res.send("Cannot find username");
         } else {
-          bcrypt.compare(
+            bcrypt.compare(
             req.body.password,
             result.password,
             function (error, isMatch) {
               if (error) {
                 throw error;
               } else if (!isMatch) {
-                console.log("Password doesn't match!");
-                res.send("Password incorrect");
+                  console.log("Password doesn't match!");
+                  res.send("Password incorrect");
               } else {
-                console.log("Password matches!");
-
-                session = req.session;
-                session.userid = req.body.username;
-                console.log(session);
-                res.redirect("/dashboard");
+                  console.log("Password matches!");
+                  let user = req.body.username;
+                  res.cookie("userData", user);
+                  res.redirect("/dashboard");
               }
             }
-          );
+            );
         }
-      });
+      });   
   });
 });
 
@@ -165,44 +153,190 @@ app.get("/logout", (req, res) => {
 });
 
 //routing calendar.js to database
-app.post("/calendar", function (req, res) {
-  db.then(function (dbc) {
+app.post("/Pub_calendar", function(req,res) {
+  db.then(function(dbc) {
     let cur_user = req.cookies.userData;
-    let cal_col = dbc.db("Boonez").collection("UserCalendars");
+    let cal_col = dbc.db("Boonez")
+    .collection("UserCalendars");
     let event = req.body;
     try {
-      const query = { user: { $eq: cur_user } };
-      cal_col.findOne(query).then((doc) => {
+      const query = { "user": {"$eq": cur_user}};
+      cal_col
+      .findOne(query).then(doc => {
         if (doc == undefined) {
-          cal_col.insertOne({ user: cur_user, eventArray: [event] });
-        } else {
-          let tmp_array = doc.eventArray;
+          cal_col.insertOne({user: cur_user, PubEventArray: [event], PriEventArray: []});
+        }
+        else {
+          let tmp_array = doc.PubEventArray;
           tmp_array.push(event);
-          cal_col.updateOne(query, { $set: { eventArray: tmp_array } });
+          cal_col.updateOne(query,
+            {$set: {"PubEventArray": tmp_array}});
         }
       });
+    } catch (err) {
+        console.log(err);
+    }
+   })
+});
+
+app.get("/Pub_calendar", function(req,res) {
+  db.then(function(dbc) {
+    try {
+      let cur_user = req.cookies.userData;
+      const query = { "user": {"$eq": cur_user}};
+      dbc.db("Boonez")
+      .collection("UserCalendars")
+      .findOne(query)
+      .then( doc => {
+        if (doc != null)
+        res.json(doc.PubEventArray);
+      });
+    } catch (err) {
+        console.log(err);
+    }
+  })
+});
+
+app.post("/Priv_calendar", function(req,res) {
+  db.then(function(dbc) {
+    let cur_user = req.cookies.userData;
+    let cal_col = dbc.db("Boonez")
+    .collection("UserCalendars");
+    let event = req.body;
+    try {
+      const query = { "user": {"$eq": cur_user}};
+      cal_col
+      .findOne(query).then(doc => {
+        if (doc == undefined) {
+          cal_col.insertOne({user: cur_user, PubEventArray: [], PriEventArray: [event]});
+        }
+        else {
+          let tmp_array = doc.PriEventArray;
+          tmp_array.push(event);
+          cal_col.updateOne(query,
+            {$set: {"PriEventArray": tmp_array}});
+        }
+      });
+    } catch (err) {
+        console.log(err);
+    }
+   })
+});
+
+app.get("/Priv_calendar", function(req,res) {
+  db.then(function(dbc) {
+    try {
+      let cur_user = req.cookies.userData;
+      const query = { "user": {"$eq": cur_user}};
+      dbc.db("Boonez")
+      .collection("UserCalendars")
+      .findOne(query)
+      .then( doc => {
+        if (doc != null) {
+          res.json(doc.PriEventArray);
+        }
+      });
+    } catch (err) {
+        console.log(err);
+    }
+  })
+});
+
+app.post("/editEvent", function(req,res) {
+  db.then(function(dbc) {
+    try {
+      let calDb = dbc.db("Boonez")
+      .collection("UserCalendars");
+      let cur_user = req.cookies.userData;
+      let eventData= req.body.data;
+      let cal_type = eventData.cal_type;
+      const query = { "user": {"$eq": cur_user}};
+      delete eventData.cal_type;
+      console.log(eventData)
+      if (cal_type == "pri"){
+        calDb.findOne(query)
+        .then(doc => {
+          let ind = doc.PriEventArray.findIndex(ele => ele.id == eventData.id);
+          doc.PriEventArray[ind] = eventData
+          calDb.replaceOne(query,doc);
+        })
+      } else {
+          calDb.findOne(query)
+          .then(doc => {
+            let ind = doc.PubEventArray.findIndex(ele => ele.id == eventData.id);
+            doc.PubEventArray[ind] = eventData
+            calDb.replaceOne(query,doc);
+          })
+      }
+
+
+    } catch (err) {
+      console.log(err);
+    }
+  })
+})
+
+
+app.delete("/deleteEvent",function(req,res) {
+  db.then(function(dbc){
+    try {
+      let calDb =       dbc.db("Boonez")
+      .collection("UserCalendars");
+
+      let cur_user = req.cookies.userData;
+      const query = { "user": {"$eq": cur_user}};
+      let eventData= req.body.data.info;
+      console.log("Event to Delete: " + eventData);
+      calDb
+      .findOne(query)
+      .then( doc => {
+        var cal = (req.body.data.cal_type == "pri") ? doc.PriEventArray : doc.PubEventArray;
+        var fil = cal.filter(function(val, ind, ar){
+          if (val.id !=eventData.id) return true;
+        })
+        console.log(fil) 
+        if (req.body.data.cal_type == "pri") {
+          calDb
+          .findOneAndUpdate(query,{$set: {PriEventArray : fil}});          } else {
+          calDb
+          .findOneAndUpdate(query,{$set: {PubEventArray : fil}});
+        } 
+      })
     } catch (err) {
       console.log(err);
     }
   });
 });
 
-app.get("/calendar", function (req, res) {
-  db.then(function (dbc) {
-    try {
-      let cur_user = req.cookies.userData;
-      const query = { user: { $eq: cur_user } };
-      dbc
-        .db("Boonez")
-        .collection("UserCalendars")
-        .findOne(query)
-        .then((doc) => {
-          // res.json(doc.eventArray); //causes a crash
-        });
-    } catch (err) {
-      console.log(err);
-    }
-  });
+app.post("/profilePicture", function(req,res) {
+  db.then(function(dbc) {
+    let cur_user = req.cookies.userData;
+    const query = { "user": {"$eq": cur_user}};
+    console.log(req.body)
+    let profdb = dbc.db("Boonez")
+    .collection("UserDashboard")
+    profdb.updateOne(query, {
+      $set: {profilePic: req.body.PicLink}},
+      {upsert: true})
+  })
+})
+
+app.get("/userDashboard", function(req,res) {
+  db.then(function(dbc) {
+    let cur_user = req.cookies.userData;
+    const query = { "user": {"$eq": cur_user}};
+    let doc = dbc.db("Boonez")
+    .collection("UserDashboard")
+    .findOne(query)
+    .then(doc => {
+      if (doc != null) {
+        console.log(doc)
+        res.json(doc);
+      } else {
+        res.json(null);
+      }
+    });
+  })
 });
 
 app.get("/styles/landingPage.css", function (req, res) {
@@ -232,15 +366,7 @@ app.get("/login", function (req, res) {
     root: __dirname,
   });
 });
-/*
-app.get("/view-feedbacks", function (req, res) {
-  db.find({})
-    .toArrary()
-    .then(function (profiles) {
-      res.status(200).json(feedbacks);
-    });
-});
-*/
+
 app.get("/images/word_logo.png", (req, res) => {
   res.sendFile("/images/word_logo.png", {
     root: __dirname,
@@ -266,11 +392,13 @@ app.get("/dashboard", (req, res) => {
     root: __dirname,
   });
 });
+
 app.get("/styles/dashboard.css", (req, res) => {
   res.sendFile("/styles/dashboard.css", {
     root: __dirname,
   });
 });
+
 app.get("/styles/messages.css", (req, res) => {
   res.sendFile(__dirname + "/styles/messages.css");
 });
@@ -285,14 +413,12 @@ app.get("/messagesOverview", (req, res) => {
 
 //used to render friends list
 app.post("/messagesOverview", (req, res) => {});
-
 /*routing to fullcalendar main.css */
 app.get("/node_modules/fullcalendar/main.css", function (req, res) {
   res.sendFile("/node_modules/fullcalendar/main.css", {
     root: __dirname,
   });
 });
-
 /*routing to fullcalendar main.js */
 app.get("/node_modules/fullcalendar/main.js", function (req, res) {
   res.sendFile("/node_modules/fullcalendar/main.js", {
@@ -303,7 +429,19 @@ app.get("/node_modules/fullcalendar/main.js", function (req, res) {
 /*routing to calendar.js*/
 app.get("/scripts/calendar.js", function (req, res) {
   res.sendFile("/scripts/calendar.js", {
-    root: __dirname,
+    root:  __dirname,
+  });
+});
+
+app.get("/styles/calendar.css", function (req, res) {
+  res.sendFile("/styles/calendar.css", {
+    root:  __dirname,
+  });
+});
+
+app.get("/scripts/dashboard.js", function (req, res) {
+  res.sendFile("/scripts/dashboard.js", {
+    root:  __dirname,
   });
 });
 
