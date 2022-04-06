554d7f34ddc6a61b0b1179796d3295b2b285f69a
Jacob Mazzarese <76662555+JacobMazzarese@users.noreply.github.com>
2022-04-05 21:19:06 -0400

    Added business dashboard js page , currently users can update profile photo and retrieve dashboard from db,

---
+++ scripts/busDashboard.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+	fetch('http://localhost:3000/businessDashboard')
+	.then(response => response.json())
+	.then(data => {
+		console.log('Success:'+ data.profilePic);
+		//let proPic = document.getElementById("profile-pic")
+		//proPic.src = data.profilePic;
+	})
+	.catch((error) => {
+		console.error("Error:" + error);
+	});
+});
+
+
+function setProf() {
+    let picLink = document.getElementById("PicLink");
+    let picSub = document.getElementById("PicSub");
+    let proPic = document.getElementById("profile-pic")
+    picSub.onclick = function() {
+        proPic.src = picLink.value;
+    }
+    let modal = document.getElementById("picModal");
+	modal.style.display = "block";
+	var span = document.getElementsByClassName("close")[0];
+	// When the user clicks on <span> (x), close the modal
+
+    span.onclick = function() {
+	modal.style.display = "none";
+	}
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+
+}
